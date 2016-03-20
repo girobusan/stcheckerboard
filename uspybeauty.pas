4becea9d4e1b681b356c3fcb3651657c4f33988c
Gleb <girobusan@gmail.com>
2016-03-20 19:18:53 +0300

    Beauty functions added

--- uspybeauty.pas
+++ uspybeauty.pas
@@ -12,21 +12,21 @@ function SpyGrouping(s:ansistring; pad:boolean=true):ansistring;
 implementation
 
 function SpyGrouping(s:ansistring; pad:boolean=true):ansistring; //Group them like a REAL spy!
-var
-   r:ansistring='';
-   i:integer;
-   p:integer=0;
-const
-  FiveHoles='00000';
-begin
-     for i:=1 to (length(s)) do
-         begin
-         r:=r+s[i];
-         if ((i mod 5) = 0) then r:=r+' ';  //constants are for sissies. Аnyway -
-         end;
-      if pad then p:= length(r) mod 5;                 //ALL spies ALL the time group them by 5.
-      SpyGrouping:=r + copy(FiveHoles, 1, p)
-end;
+  var
+     r:ansistring='';
+     i:integer;
+     p:integer=0;
+  const
+    FiveBullets='00000';
+  begin
+       for i:=1 to (length(s)) do
+           begin
+           r:=r+s[i];
+           if ((i mod 5) = 0) then r:=r+' ';  //constants are for sissies. Аnyway -
+           end;                              //ALL spies ALL the time group them by 5.
+        if pad then p:= length(r) mod 5;
+        SpyGrouping:=r + copy(FiveBullets, 1, p)
+  end;
 
 end.
 
